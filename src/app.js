ba34b009248f9c589f3e25180f99e5373ce9d6ba
Matheus Lira <matheustlira@gmail.com>
2021-07-12 14:53:18 -0300

    Post to /list working and validating

--- src/app.js
+++ src/app.js
@@ -1,10 +1,39 @@
 import express from "express";
 import cors from "cors";
+import joi from "joi";
 
 import connection from "./database.js";
 
 const app = express();
 app.use(express.json());
 app.use(cors());
+
+app.post("/list", async (req, res) => {
+  try {
+    const validationSchema = joi.object({
+      text: joi.string().required().min(2),
+    });
+
+    const validation = validationSchema.validate(req.body);
+
+    if (validation.error) {
+      console.log(validation.error);
+      return res.sendStatus(400);
+    }
+    const { text } = req.body;
+    await connection.query(
+      `
+      INSERT INTO list
+      (item)
+      VALUES ($1)
+      `,
+      [text]
+    );
+    res.sendStatus(201);
+  } catch (e) {
+    console.log(e);
+    res.sendStatus(500);
+  }
+});
 
 export default app;
